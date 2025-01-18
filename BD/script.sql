3444859eb97647788b6c6f129ccb35fb14df8b63
CarolFeltz <anafeltz05@gmail.com>
2025-01-18 13:26:47 -0300

    commit: save bd

--- BD/script.sql
+++ BD/script.sql
@@ -1,16 +1,35 @@
-create if not exist table ESTOQUE_CLIENTE{
+create table if not exists ESTOQUE_CLIENTE(
     id_cliente int auto_increment,
     primary key(id_cliente)
-};
+);
 
-create if not exist table ESTOQUE_VENDA{
+create table if not exists ESTOQUE_VENDA(
+    id_venda int auto_increment,
+    data_venda date not null,
+    forma_pagamento varchar(45) not null,
+    valor_total double not null,
+    id_cliente int not null,
+    primary key(id_venda),
+    foreign key(id_cliente) references ESTOQUE_CLIENTE(id_cliente)
+);
 
-};
+create table if not exists ESTOQUE_PRODUTO(
+    id_produto int auto_increment,
+    nome varchar(255) not null,
+    descricao text not null,
+    preco double not null,
+    quantidade_estoque int not null,
+    primary key(id_produto)
+);
 
-create if not exist table ESTOQUE_PRODUTO{
-
-};
-
-create if not exist table ESTOQUE_ITEM_VENDA{
-
-};
+create table if not exists ESTOQUE_ITEM_VENDA(
+    id_item_venda int auto_increment,
+    quantidade int not null,
+    preco_unitario double not null,
+    subtotal double not null,
+    id_venda int not null,
+    id_produto int not null,
+    primary key(id_item_venda),
+    foreign key(id_venda) references ESTOQUE_VENDA(id_venda),
+    foreign key(id_produto) references ESTOQUE_PRODUTO(id_produto) 
+);
