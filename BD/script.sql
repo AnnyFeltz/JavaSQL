fa22e5d8cfe518f690361a904c8382973fed9f9d
CarolFeltz <anafeltz05@gmail.com>
2025-02-01 16:39:36 -0300

    commit: adicionei o povoamento do exmplo no BD

--- BD/script.sql
+++ BD/script.sql
@@ -27,3 +27,26 @@ create table if not exists ESTOQUE_ITEM_VENDA(
     foreign key(id_venda) references ESTOQUE_VENDA(id_venda),
     foreign key(id_produto) references ESTOQUE_PRODUTO(id_produto) 
 );
+
+/*pro exemplo*/
+INSERT INTO ESTOQUE_VENDA (id_venda, data_venda, forma_pagamento, valor_total, id_cliente)
+VALUES
+(11, '2025-02-01', 'Dinheiro', 126.00, 1);
+
+INSERT INTO ESTOQUE_PRODUTO (id_produto, nome, preco, descricao, quantidade_estoque) VALUES (31, 'Maçã', 5.00, 'pacote de 1kg', 67);
+
+INSERT INTO ESTOQUE_ITEM_VENDA (quantidade, preco_unitario, subtotal, id_venda, id_produto)
+VALUES
+(2, 5.00, 10.00, 11, 31);
+
+INSERT INTO ESTOQUE_ITEM_VENDA (quantidade, preco_unitario, subtotal, id_venda, id_produto)
+VALUES
+(2, 35.00, 70.00, 11, 1);
+
+INSERT INTO ESTOQUE_ITEM_VENDA (quantidade, preco_unitario, subtotal, id_venda, id_produto)
+VALUES
+(1, 12.00, 12.00, 11, 2);
+
+UPDATE ESTOQUE_VENDA
+SET valor_total = (SELECT SUM(subtotal) FROM ESTOQUE_ITEM_VENDA WHERE id_venda = 11)
+WHERE id_venda = 11;
